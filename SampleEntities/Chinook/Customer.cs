fd98503fded8363753adf4ddf3302a6d98d55c7d
Barbara Post <barbara.post@live.be>
2014-07-17 12:53:53 +0200

    ORM-41 : ajout d'un projet pour contenir des DbEntities. Je l'ai nommé SampleEntities et organisé par namespace avec le nom d'une base de données pour namespace.

---
+++ SampleEntities/Chinook/Customer.cs
@@ -0,0 +1,30 @@
+﻿/*
+This file is part of OSAMES Micro ORM.
+Copyright 2014 OSAMES
+
+OSAMES Micro ORM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OSAMES Micro ORM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OSAMES Micro ORM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace SampleEntities.Chinook
+{
+    /// <summary>
+    /// Objet/table "Customer" de la base de données Chinook.
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Id Customer.
+        /// </summary>
+        public int IdCustomer { get; set; }
+    }
+}
