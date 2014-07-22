2c8dfb99fd131ae309d910448d5024a469f08075
Barbara Post <barbara.post@live.be>
2014-07-22 13:31:44 +0200

    ORM-37 : ajout partiel de TestOsamesMicroOrmSqlite pour pouvoir faire le TU qu'on veut faire.

--- OsamesMicroOrm/DbManager.cs
+++ OsamesMicroOrm/DbManager.cs
@@ -1,23 +1,20 @@
+/*
+This file is part of OSAMES Micro ORM.
+Copyright 2014 OSAMES
 
-#region COPYRIGHTS
+OSAMES Micro ORM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-/*	
+OSAMES Micro ORM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    - - - - - - - - - - - - - - - - - - - - - - -
-    File Name : DBHelper.cs
-    - - - - - - - - - - - - - - - - - - - - - - -
-
-    System				    :  	
-    Module				    :  	Data Layer
-    Author				    :	Shyam SS
-    Date					:	28 June 2007
-    Function				:	Define the Data Wrapper Class which provides the functionalities of DBProvider Factory.
-    Desctiption	            :   
-
-    
- */
-
-#endregion
+You should have received a copy of the GNU Affero General Public License
+along with OSAMES Micro ORM.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 using System;
 using System.Collections.Generic;
