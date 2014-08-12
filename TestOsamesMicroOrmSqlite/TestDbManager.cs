df8b92355e62af86e0996fea04bce1f775353e5b
Barbara Post <barbara.post@live.be>
2014-08-12 13:27:21 +0200

    ORM-36 : réintégration de deux tests dans TestDbManager (projet de TU Sqlite). TU Select au vert, TU Insert au rouge.

--- TestOsamesMicroOrmSqlite/TestDbManager.cs
+++ TestOsamesMicroOrmSqlite/TestDbManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsamesMicroOrm;
 
 namespace TestOsamesMicroOrmSqlite
 {
@@ -30,7 +33,63 @@ namespace TestOsamesMicroOrmSqlite
             Assert.IsNotNull(provider);
         }
 
-        // TODO les différents tests seront à réintégrer ici.
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [Owner("Barbara Post")]
+        [TestCategory("Sql")]
+        [TestCategory("SqLite")]
+        [TestCategory("Sql Select")]
+        [TestCategory("Sql execution")]
+        public void TestSelectUsingSqlite()
+        {
+            // select * from clients where id_client = @p0
+            List<DbManager.Parameter> parameters = new List<DbManager.Parameter> { new DbManager.Parameter("@customerid", 3) };
+
+            object idCustomer =  null, lastName = null;
+
+            using (IDataReader reader = DbManager.Instance.ExecuteReader("SELECT * FROM Customer WHERE CustomerId = @customerid", parameters.ToArray()))
+            {
+                if (reader.Read())
+                {
+                    idCustomer = reader["CustomerId"];
+                    Console.WriteLine("Customer ID: {0}", idCustomer);
+                    lastName = reader["LastName"];
+                    Console.WriteLine("Last name: {0}", lastName);
+                }
+            }
+            Assert.IsNotNull(idCustomer);
+            Assert.AreEqual(3, int.Parse(idCustomer.ToString()));
+            Assert.IsNotNull(lastName);
+            Assert.AreEqual("Tremblay", lastName.ToString());
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [Owner("Benjamin Nolmans")]
+        [TestCategory("Sql")]
+        [TestCategory("SqLite")]
+        [TestCategory("Sql Insert")]
+        [TestCategory("Sql execution")]
+        public void TestInsertUsingSqlite()
+        {
+            List<DbManager.Parameter> parameters = new List<DbManager.Parameter> { new DbManager.Parameter("@lastname", "test customer") };
+
+            try
+            {
+                long lastInsertedRowId;
+                int affectedRecordsCount = DbManager.Instance.ExecuteNonQuery("INSERT INTO Customer (LastName) VALUES (@lastname)", parameters.ToArray(), out lastInsertedRowId, CommandType.Text, _transaction);
+
+                Assert.AreEqual(1, affectedRecordsCount, "Expected 1 record affected by INSERT operation");
+                Console.WriteLine("New record ID: {0}, expected number > 1", lastInsertedRowId);
+                Assert.AreNotEqual(0, lastInsertedRowId);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                // relancer l'exception telle que catchée comme ça le test est correctement en erreur
+                throw;
+            }
+        }
       
         /// <summary>
          /// From http://msdn.microsoft.com/en-us/library/system.data.datatable(v=vs.110).aspx
