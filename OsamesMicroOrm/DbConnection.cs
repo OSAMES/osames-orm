96944f0e7e867e08a24b7ca77161558cac2a65fe
Barbara Post <barbara.post@live.be>
2014-10-14 22:19:46 +0200

    ORM-94 : fin de création des classes DbConnection et DbTransaction de l'ORM.

--- OsamesMicroOrm/DbConnection.cs
+++ OsamesMicroOrm/DbConnection.cs
@@ -57,105 +57,171 @@ namespace OsamesMicroOrm
 
         #region reprise des propriétés et méthodes publiques de System.Data.Common.DbConnection
 
-        // TODO copier les summary depuis http://msdn.microsoft.com/fr-fr/library/system.data.common.dbconnection(v=vs.110).aspx
-
-        public System.Data.Common.DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+        /// <summary>
+        /// Commence une transaction de base de données.
+        /// </summary>
+        /// <param name="isolationLevel"></param>
+        /// <returns></returns>
+        public DbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
         {
-            // TODO remplacer avec un OsamesMicroOrm.DbTransaction
-            return AdoDbConnection.BeginTransaction(isolationLevel);
+            return new DbTransaction (AdoDbConnection.BeginTransaction(isolationLevel)) { Connection = this};
         }
 
-        public System.Data.Common.DbTransaction BeginDbTransaction()
+        /// <summary>
+        /// Commence une transaction de base de données.
+        /// </summary>
+        /// <returns></returns>
+        public DbTransaction BeginDbTransaction()
         {
-            // TODO remplacer avec un OsamesMicroOrm.DbTransaction
-            return AdoDbConnection.BeginTransaction();
+
+            return new DbTransaction(AdoDbConnection.BeginTransaction()) { Connection = this };
         }
 
+        /// <summary>
+        /// Ferme la connexion à la base de données. C'est la méthode recommandée de fermeture d'une connexion ouverte.
+        /// </summary>
         public void Close()
         {
             AdoDbConnection.Close();
         }
 
+        /// <summary>
+        /// Libère toutes les ressources utilisées.
+        /// </summary>
         public void Dispose()
         {
             AdoDbConnection.Dispose();
         }
 
+        /// <summary>
+        /// S'inscrit dans la transaction spécifiée.
+        /// </summary>
+        /// <param name="transaction_"></param>
         public void EnlistTransaction(Transaction transaction_)
         {
             AdoDbConnection.EnlistTransaction(transaction_);
         }
 
+        /// <summary>
+        /// Retourne les informations de schéma pour la source de données de ce DbConnection.
+        /// </summary>
+        /// <returns></returns>
         public DataTable GetSchema()
         {
             return AdoDbConnection.GetSchema();
         }
 
+        /// <summary>
+        /// Retourne des informations de schéma pour la source de données de ce DbConnection à l'aide de la chaîne spécifiée pour le nom de schéma.
+        /// </summary>
+        /// <param name="name_"></param>
+        /// <returns></returns>
         public DataTable GetSchema(string name_)
         {
             return AdoDbConnection.GetSchema(name_);
         }
 
+        /// <summary>
+        /// Retourne des informations de schéma pour la source de données de ce DbConnection à l'aide de la chaîne spécifiée pour le nom de schéma et du tableau de chaînes spécifié pour les valeurs de restriction.
+        /// </summary>
+        /// <param name="name_"></param>
+        /// <param name="restrictions_"></param>
+        /// <returns></returns>
         public DataTable GetSchema(string name_, string[] restrictions_)
         {
             return AdoDbConnection.GetSchema(name_, restrictions_);
         }
 
-        public Type GetType()
-        {
-            return AdoDbConnection.GetType();
-        }
-
+        /// <summary>
+        /// Modifie la base de données active d'une connexion ouverte.
+        /// </summary>
+        /// <param name="databaseName"></param>
         public void ChangeDatabase(string databaseName)
         {
             AdoDbConnection.ChangeDatabase(databaseName);
         }
 
+        /// <summary>
+        /// Ouvre une connexion à une base de données avec les paramètres spécifiés par ConnectionString.
+        /// </summary>
         public void Open()
         {
             AdoDbConnection.Open();
         }
 
+        /// <summary>
+        /// Version asynchrone de Open, qui ouvre une connexion de base de données avec les paramètres spécifiés par ConnectionString. Cette méthode appelle la méthode virtuelle OpenAsync avec CancellationToken.None. 
+        /// </summary>
         public void OpenAsync()
         {
             AdoDbConnection.OpenAsync();
         }
 
+        /// <summary>
+        /// Il s'agit de la version asynchrone de Open.
+        /// </summary>
+        /// <param name="token_"></param>
         public void OpenAsync(CancellationToken token_)
         {
             AdoDbConnection.OpenAsync(token_);
         }
 
+        /// <summary>
+        /// Obtient ou définit la chaîne utilisée pour ouvrir la connexion.
+        /// </summary>
         public string ConnectionString { get { return AdoDbConnection.ConnectionString; } set { AdoDbConnection.ConnectionString = value; } }
 
+        /// <summary>
+        /// Obtient la durée d'attente préalable à l'établissement d'une connexion avant que la tentative ne soit abandonnée et qu'une erreur ne soit générée.
+        /// </summary>
         public int ConnectionTimeout { get { return AdoDbConnection.ConnectionTimeout; } }
 
+        /// <summary>
+        /// Obtient le nom de la base de données active après avoir ouvert une connexion, ou le nom de la base de données spécifié dans la chaîne de connexion avant que la connexion ne soit ouverte.
+        /// </summary>
         public string Database
         {
             get { return AdoDbConnection.Database; }
         }
 
+        /// <summary>
+        /// Obtient une chaîne qui décrit l'état de la connexion.
+        /// </summary>
         public ConnectionState State
         {
             get { return AdoDbConnection.State; }
         }
 
+        /// <summary>
+        /// Obtient le nom du serveur de base de données auquel se connecter.
+        /// </summary>
         public string DataSource
         {
             get { return AdoDbConnection.DataSource; }
         }
 
+        /// <summary>
+        /// Obtient une chaîne qui représente la version du serveur auquel l'objet est connecté.
+        /// </summary>
         public string ServerVersion
         {
             get { return AdoDbConnection.ServerVersion; }
         }
 
+        /// <summary>
+        /// Crée et retourne un objet DbCommand associé à la connexion active.
+        /// </summary>
+        /// <returns></returns>
         protected DbCommand CreateDbCommand()
         {
             return AdoDbConnection.CreateCommand();
         }
 
-        public string ToString()
+        /// <summary>
+        /// Chaîne représentative de l'objet courant.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
         {
             return IsBackup ? "[BACKUP]" : "[POOLED]" + AdoDbConnection;
         }
--- OsamesMicroOrm/DbTransaction.cs
+++ OsamesMicroOrm/DbTransaction.cs
@@ -1,15 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+This file is part of OSAMES Micro ORM.
+Copyright 2014 OSAMES
+
+OSAMES Micro ORM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OSAMES Micro ORM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OSAMES Micro ORM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 using System.Data;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OsamesMicroOrm
 {
-    public class DbTransaction : System.Data.Common.DbConnection
+    /// <summary>
+    /// Wrapper de la classe System.Data.Common.DbTransaction pour gérer une référence vers la classe DbConnection de l'ORM.
+    /// Elle expose les mêmes méthodes que System.Data.Common.DbTransaction à qui elle délègue.
+    /// </summary>
+    public class DbTransaction
     {
         
+        /// <summary>
+        /// Connexion telle que fournie par l'appel à DbProviderFactory.CreateConnection().
+        /// Accessible en interne pour l'ORM.
+        /// </summary>
+        internal System.Data.Common.DbTransaction AdoTransaction { get; private set; }
+
+        /// <summary>
+        /// Constructeur.
+        /// </summary>
+        /// <param name="adoTransaction_">DbTransaction ADO.NET</param>
+        internal DbTransaction(System.Data.Common.DbTransaction adoTransaction_)
+        {
+            AdoTransaction = adoTransaction_;
+        }
+
+        /// <summary>
+        /// Libère les ressources non managées utilisées.
+        /// </summary>
+        public void Dispose()
+        {
+            AdoTransaction.Dispose();
+        }
+
+        /// <summary>
+        /// Valide la transaction de base de données.
+        /// </summary>
+        public void Commit()
+        {
+            AdoTransaction.Commit();
+        }
+
+        /// <summary>
+        /// Restaure une transaction à partir d'un état d'attente.
+        /// </summary>
+        public void Rollback()
+        {
+            AdoTransaction.Rollback();
+        }
+
+        /// <summary>
+        /// Connexion (classe définie dans l'ORM)
+        /// </summary>
+        public DbConnection Connection { get; internal set; }
+
+        /// <summary>
+        /// Niveau d'isolation de la transaction.
+        /// </summary>
+        public IsolationLevel IsolationLevel { get { return AdoTransaction.IsolationLevel; } }
     }
 }
