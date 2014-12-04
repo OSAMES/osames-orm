8d5dad444fcc82264fa2395a31aa2bf0bdcfc260
Benjamin Nolmans <benjamin.nolmans@osames.org>
2014-12-04 12:11:08 +0100

    ORM-87 ajout de la partie qui permet de lire les HResult embarqués dans la dll et de placer le tout dans un dictionnaire.

--- OsamesMicroOrm/Utilities/OOrmErrorsHandler.cs
+++ OsamesMicroOrm/Utilities/OOrmErrorsHandler.cs
@@ -18,12 +18,55 @@ along with OSAMES Micro ORM.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace OsamesMicroOrm.Utilities
 {
     internal static class ErrorsHandler
     {
         private static KeyValuePair<ErrorType, string> ErrorMsg;
+        internal static Dictionary<string, string> HResultCode = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Cosntructor
+        /// </summary>
+        public static void ErrorEventHandler()
+        {
+            ReadHResultCodeFromResources("HResult Orm.csv", out HResultCode);
+        }
+
+        private static void ReadHResultCodeFromResources(string resource_, out Dictionary<string, string> hresultCodes_)
+        {
+            hresultCodes_ = new Dictionary<string, string>();
+
+            using (Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream(typeof(ErrorsHandler).Assembly.GetName().Name + ".Resources." + resource_))
+            {
+                //null if resource doesn't exist
+                if (str == null)
+                    return;
+
+                string[] row = new string[2];
+
+                using (StreamReader sr = new StreamReader(str))
+                {
+                    //Skip csv header
+                    sr.ReadLine();
+
+                    string currentLine;
+                    // currentLine will be null when the StreamReader reaches the end of file
+
+                    while ((currentLine = sr.ReadLine()) != null)
+                    {
+                        //Insert to kvp
+                        row = currentLine.Split(';');
+                        if (string.IsNullOrWhiteSpace(row[1]))
+                            continue;
+                        hresultCodes_.Add(row[1], row[0]);
+                    }
+                }
+            }
+        }
 
         /// <summary>
         /// 
