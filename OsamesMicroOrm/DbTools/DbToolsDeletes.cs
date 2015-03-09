fdf1f0351e9800f40297dd318d22493e06908030
Barbara Post <bpo@eonix.be>
2015-03-09 22:51:42 +0100

    ORM-144 : revue de code/summary et ajout de divers tests unitaires.

--- OsamesMicroOrm/DbTools/DbToolsDeletes.cs
+++ OsamesMicroOrm/DbTools/DbToolsDeletes.cs
@@ -26,38 +26,29 @@ using OsamesMicroOrm.Logging;
 namespace OsamesMicroOrm.DbTools
 {
     /// <summary>
-    /// TODO
+    /// Classe servant à formater et exécuter des requêtes SQL de type DELETE, en proposant une abstraction au dessus de ADO.NET.
     /// </summary>
     public class DbToolsDeletes
     {
 
         /// <summary>
-        /// Crée le texte de la commande SQL paramétrée ainsi que les paramètres ADO.NET, dans le cas d'un update sur un seul objet.
-        /// Utilise le template "BaseUpdate" : <c>"UPDATE {0} SET {1} WHERE {2}"</c> ainsi que les éléments suivants :
+        /// Crée le texte de la commande SQL paramétrée ainsi que les paramètres ADO.NET, dans le cas d'un delete.
+        /// Utilise des templates de type <c>"DELETE FROM {0} WHERE ..."</c> ainsi que les éléments suivants :
         /// <list type="bullet">
         /// <item><description>clé du dictionnaire de mapping</description></item>
-        /// <item><description>un objet de données dataObject_</description></item>
-        /// <item><description>liste de noms de propriétés de dataObject_ à utiliser pour les champs à mettre à jour</description></item>
-        /// <item><description>nom de la propriété de dataObject_ correspondant au champ clé primaire</description></item>
         /// </list>
         /// </summary>
-        /// <typeparam name="T">Type C#</typeparam>
-        /// <param name="dataObject_">Instance d'un objet de la classe T</param>
         /// <param name="mappingDictionariesContainerKey_">Clé pour le dictionnaire de mapping</param>
         /// <param name="sqlTemplate_">Contient le nom du template sql update à utiliser</param>
-        /// <param name="lstDataObjectColumnNames_">Noms des propriétés de l'objet dataObject_ à utiliser pour les champs à mettre à jour</param>
         /// <param name="lstWhereMetaNames_">Pour les colonnes de la clause where : indication d'une propriété de dataObject_ ou un paramètre dynamique. 
         /// Pour formater à partir de {2} dans le template SQL. Peut être null</param>
         /// <param name="lstWhereValues_">Valeurs pour les paramètres ADO.NET. Peut être null</param>
         /// <param name="sqlCommand_">Sortie : texte de la commande SQL paramétrée</param>
         /// <param name="lstAdoParameters_">Sortie : clé/valeur des paramètres ADO.NET pour la commande SQL paramétrée</param>
-        /// <param name="tryFormat">Si a vrai, on fait un try format sur le sqlcommand</param>
-        /// <returns>Ne renvoie rien</returns>
+        ///  <returns>Ne renvoie rien</returns>
         /// <exception cref="OOrmHandledException">Toute sorte d'erreur</exception>
         internal static void FormatSqlForDelete(string sqlTemplate_, string mappingDictionariesContainerKey_, List<string> lstWhereMetaNames_, List<object> lstWhereValues_, out string sqlCommand_, out List<KeyValuePair<string, object>> lstAdoParameters_)
         {
-            StringBuilder sbFieldsToUpdate = new StringBuilder();
-            sqlCommand_ = null;
             lstAdoParameters_ = new List<KeyValuePair<string, object>>();
 
             // 2. Positionne le premier placeholders
@@ -72,10 +63,7 @@ namespace OsamesMicroOrm.DbTools
 
         /// <summary>
         /// Exécute une requête de type "DELETE FROM {0} WHERE ...".
-        /// TODO
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="dataObject_"></param>
         /// <param name="refSqlTemplate_"></param>
         /// <param name="mappingDictionariesContainerKey_"></param>
         /// <param name="lstWhereMetaNames_"></param>
@@ -118,8 +106,6 @@ namespace OsamesMicroOrm.DbTools
                 if (!conn.IsBackup)
                     conn.Dispose();
             }
-
-            return 0;
         }
     }
 }
---
+++ TestOsamesMicroOrmMsSql/TestDbToolsDeletes.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsamesMicroOrm;
+using OsamesMicroOrm.DbTools;
+using SampleDbEntities.Chinook;
+using TestOsamesMicroOrm.Tools;
+
+namespace TestOsamesMicroOrmMsSql
+{
+    /// <summary>
+    /// Tests unitaires de haut niveau des méthodes d'exécution d'instructions SQL DELETE, avec formatage de la requête et des paramètres ADO.NET.
+    /// But : test complet de l'ORM.
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class TestDbToolsDeletes : OsamesMicroOrmMsSqlTest
+    {
+
+        /// <summary>
+        /// Delete d'un objet inexistant.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("MsSql")]
+        [TestCategory("Delete")]
+        public void TestDelete()
+        {
+            Customer toto = new Customer {FirstName = "toto", LastName = "toto", Email = "hop@test.com"};
+            DbToolsInserts.Insert(toto, "BaseInsert", "Customer", new List<string> {"FirstName", "LastName", "Email"}, _transaction);
+            long nb = DbToolsDeletes.Delete("BaseDeleteWhere", "Customer", new List<string> { "FirstName", "#" }, new List<object> { "toto" }, _transaction);
+            Assert.AreEqual(1, nb, "1 ligne doit être effacée");
+        }
+
+        /// <summary>
+        /// Delete d'un objet existant. Blocage par une FK.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("MsSql")]
+        [TestCategory("Delete")]
+        [ExpectedException(typeof(OOrmHandledException))]
+        public void TestDeleteForbidden()
+        {
+            try
+            {
+                long nb = DbToolsDeletes.Delete("BaseDeleteWhere", "Customer", new List<string> {"IdCustomer", "#"}, new List<object> {3}, _transaction);
+                Assert.AreEqual(1, nb, "1 ligne doit être effacée");
+            }
+            catch (OOrmHandledException ex)
+            {
+                Common.AssertOnHresultAndWriteToConsole(HResultEnum.E_EXECUTENONQUERYFAILED, ex);
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Delete d'un objet inexistant.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("MsSql")]
+        [TestCategory("Delete")]
+        public void TestDeleteNoMatch()
+        {
+            long nb = DbToolsDeletes.Delete("BaseDeleteWhere", "Customer", new List<string> { "IdCustomer", "#" }, new List<object> { -1 }, _transaction);
+            Assert.AreEqual(0, nb, "0 ligne doit être effacée");
+        }
+
+
+    }
+}
