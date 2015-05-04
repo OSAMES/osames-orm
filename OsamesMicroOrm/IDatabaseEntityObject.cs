0818f554fe9ff9d0b37ec22e48227c8dac6b7a11
Benjamin Nolmans <benjamin.nolmans@osames.org>
2015-05-04 09:53:42 +0200

    ORM-171

--- OsamesMicroOrm/IDatabaseEntityObject.cs
+++ OsamesMicroOrm/IDatabaseEntityObject.cs
@@ -1,4 +1,22 @@
-﻿namespace OsamesMicroOrm
+﻿/*
+This file is part of OSAMES Micro ORM.
+Copyright 2014 OSAMES
+
+OSAMES Micro ORM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OSAMES Micro ORM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OSAMES Micro ORM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace OsamesMicroOrm
 {
     /// <summary>
     /// Interface à utiliser si on ne peut dériver de la classe abstraite DatabaseEntityObject qui est optimisée.
